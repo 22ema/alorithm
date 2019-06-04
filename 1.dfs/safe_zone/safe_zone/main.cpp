1465fe7a70d29e1fe20e9b2edf9d5499332c4327
22ema <yunsung9503@gmail.com>
2019-06-04 15:52:12 +0900

    safe_zone_update
    세이프존 문제 해결

--- 1.dfs/safe_zone/safe_zone/main.cpp
+++ 1.dfs/safe_zone/safe_zone/main.cpp
@@ -10,27 +10,31 @@ int result;
 int Count[101];
 using namespace std;
 
-void dfs(int start, int start_second,int thresh) {
+void dfs(int start, int start_second, int thresh) {
 	if (zone[start][start_second] >= thresh && is_visited[start][start_second] == 0) {
 		is_visited[start][start_second] = 1;
 		t_count++;
+
 	}
 	if (zone[start][start_second] >= thresh) {
 		if (zone[start + 1][start_second] >= thresh && is_visited[start + 1][start_second] == 0) {
 			dfs(start + 1, start_second, thresh);
 			is_visited[start + 1][start_second] = 1;
 			t_count++;
+
 		}
 		if (zone[start][start_second + 1] >= thresh && is_visited[start][start_second + 1] == 0) {
 			dfs(start, start_second + 1, thresh);
 			is_visited[start][start_second + 1] = 1;
 			t_count++;
+
 		}
 		if (start > 0) {
 			if (zone[start - 1][start_second] >= thresh && is_visited[start - 1][start_second] == 0) {
 				dfs(start - 1, start_second, thresh);
 				is_visited[start - 1][start_second] = 1;
 				t_count++;
+
 			}
 		}
 		if (start_second > 0) {
@@ -38,34 +42,35 @@ void dfs(int start, int start_second,int thresh) {
 				dfs(start, start_second - 1, thresh);
 				is_visited[start][start_second - 1] = 1;
 				t_count++;
+
 			}
 		}
 	}
-	
+
 }
 
 int main() {
-	
+
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> zone[i][j];
-			
+
 		}
-		
+
 	}
 	Count[0] = 1;
-	
-	for(int k = 1; k < 100; k++){
-		int Count_s=0;
-		
+
+	for (int k = 1; k < N_MAX; k++) {
+		int Count_s = 0;
+
 		memset(is_visited, 0, sizeof(is_visited));
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (is_visited[N_MAX][N_MAX] == 0) {
+				if (is_visited[i][j] == 0) {
 					t_count = 0;
-					dfs(i, j,k);
-					
+					dfs(i, j, k);
+
 					if (t_count > 0) {
 						Count_s++;
 					}
@@ -78,9 +83,9 @@ int main() {
 			break;
 		}
 	}
-	
+
 	int max = Count[0];
-	for (int i = 1; i < sizeof(Count); i++) {
+	for (int i = 1; i < N_MAX; i++) {
 		if (max < Count[i]) {
 			max = Count[i];
 		}
